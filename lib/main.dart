40cf214bdd3e1f51a96f3cb718da0bbaca7cca46
ghosty47 <ghostcybertron@gmail.com>
2024-02-09 13:51:06 +0100

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter mines',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -70,18 +70,13 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
         title: Text(widget.title),
       ),
       body: Center(
-
         child: Column(
- 
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
